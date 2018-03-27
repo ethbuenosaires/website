57e41a9c1f0a4fe792e5ecb3fc283c7aa19b3f51
Delfina <delfinapolito@gmail.com>
2018-03-27 11:34:43 -0300

    sidenav behaviour

--- js/home.js
+++ js/home.js
@@ -1,4 +1,9 @@
 $( document ).ready(function(){
   $(".button-collapse").sideNav();
+  
+  $('.sidenav-close').on( "click", function() {
+	  $('.button-collapse').sideNav('hide');
+	});
+  
   $('.modal').modal();
 })
